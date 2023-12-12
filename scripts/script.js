153f3fc63198d70f7ef76f59554e648c94b02086
Nielsadp <147333204+Nielsadp@users.noreply.github.com>
2023-12-12 16:52:43 +0100

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,16 @@
-// JavaScript Document
-console.log("hi");
+
+var HamburgerMenu = document.querySelector("button");
+var menu = document.querySelector("nav");
+HamburgerMenu.onclick = openMenu;
+function openMenu() {
+menu.classList.add("showMenu");
+  console.log("hij werkt");
+}
+
+var MenuCloseButton = document.querySelector("nav button");
+MenuCloseButton.onclick = closeMenu;
+function closeMenu() {
+  var menu = document.querySelector("nav");
+  menu.classList.remove("showMenu");
+  console.log("hij werkt");
+}
